6efea87578c6567160f7dfe2e5a5973f6d23f8f9
luciano <luciano.geronimo.fnord@gmail.com>
2017-08-11 16:35:32 -0300

    Não vou continuar a carga do arquivo. Ela é complicada e nada acrescenta (no momento).

--- volumeRenderer.cpp
+++ volumeRenderer.cpp
@@ -4,7 +4,50 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include <glm/gtx/transform.hpp>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <cctype>
+#include <locale>
+#include <regex>
+#include <array>
+#include <ctime>
+#include <chrono>
+///////ESSAS COISAS AQUI DEVERIAM ESTAR EM UM UTILS.H
+static inline std::string &ltrim(std::string &s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	return s;
+}
 
+// trim from end
+static inline std::string &rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	return s;
+}
+
+// trim from both ends
+static inline std::string &trim(std::string &s) {
+	return ltrim(rtrim(s));
+}
+static inline  std::vector<std::string> SplitString(const std::string& input, const std::string& regex) {
+	// passing -1 as the submatch index parameter performs splitting
+	std::regex re(regex);
+	std::sregex_token_iterator
+		first{ input.begin(), input.end(), re, -1 },
+		last;
+	return{ first, last };
+}
+//Current time as string
+static inline std::string GetDateAsString()
+{
+	std::chrono::time_point<std::chrono::system_clock> hora = std::chrono::system_clock::now();
+	std::time_t end_time = std::chrono::system_clock::to_time_t(hora);
+	std::string t(std::ctime(&end_time));
+	t = trim(t);
+	return t;
+}
 void teste_opengl()
 {
 	GLenum err = glGetError();
@@ -15,11 +58,13 @@ void teste_opengl()
 		throw std::exception(ss.str().c_str());
 	}
 }
+//---------------------------------------------
 void VolumeRenderer::Rotate(array<double, 3> vector, double angInDegs)
 {
 	const glm::vec3 v = glm::vec3(vector[0], vector[1], vector[2]);
 	modelMatrix = glm::rotate(glm::radians<float>(angInDegs), v);
 }
+
 
 VolumeRenderer::VolumeRenderer()
 {
@@ -63,6 +108,47 @@ VolumeRenderer::VolumeRenderer()
 	glEnableVertexAttribArray(positionLocation);
 	glEnableVertexAttribArray(colorLocation);
 	glUseProgram(0);
+	//Leitura do arquivo
+	const std::string path_do_cubo = "C:/Nova pasta (2)/volume_rendering_cpp/cubo.obj";
+	std::vector<std::string> linhasDoArquivo;
+	ifstream myReadFile;
+	myReadFile.open(path_do_cubo);
+	if (!myReadFile.is_open()) throw std::exception("não foi possivel abrir o arquivo");
+	std::string temp;
+	while (std::getline(myReadFile,temp))
+	{
+		linhasDoArquivo.push_back(temp);
+	}
+	std::vector<std::array<float, 3>> tempVecList;
+	std::vector < std::array<unsigned int, 3>> tempTrianglePointsList;
+	for (std::string ln : linhasDoArquivo)
+	{
+		std::vector<std::string> splittedLine = SplitString(ln, " ");
+		if (splittedLine[0] == "o")//Objeto
+			std::cout << "Nome do objeto = " << splittedLine[1] << std::endl;
+		if (splittedLine[0] == "v") //Vertices
+		{
+			std::array<float, 3> point = { { stof(splittedLine[1]), stof(splittedLine[2]), stof(splittedLine[3]) } };
+			tempVecList.push_back(point);
+		}
+		if (splittedLine[0] == "f")//definição de uma face
+		{
+			std::vector<std::string> t1 = SplitString(splittedLine[1], "//");
+			std::vector<std::string> t2 = SplitString(splittedLine[2], "//");
+			std::vector<std::string> t3 = SplitString(splittedLine[2], "//");
+			std::array<unsigned int, 3> verts = { {
+					stoi(t1[0]), stoi(t2[0]), stoi(t3[0]),
+				} };
+			tempTrianglePointsList.push_back(verts);
+		}
+	}
+	//Percorre a lista temporária das faces e as adiciona à lista permanente (vertexes)
+	for (array<unsigned int, 3> currentTemp : tempTrianglePointsList)
+	{
+		std::array<float, 3> v0
+	}
+
+
 	//Criação da geometria
 	vertexes.push_back(-1.0f); vertexes.push_back(1.0f); vertexes.push_back(1.0f);
 	vertexes.push_back(1.0f); vertexes.push_back(-1.0f); vertexes.push_back(1.0f);
